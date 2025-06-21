ccddc1d855cdc6758943fa00ca7976d9d3402650
Staphylococcus <softcupcaketeam@gmail.com>
2025-06-21 22:20:37 +0900

    Add files via upload
    Roughly create a scheduler
    I think we need to improve it

---
+++ ANK/modules/scheduler/scheduler.c
@@ -0,0 +1,10 @@
+#include "./scheduler.h"
+#include "../work_system/work_system.h"
+
+void sch64(ANK_WORK *works, size64 numof_work)   {
+    //work loop
+    for (size64 i = 0; i < numof_work; i++) {
+        works[i];
+    }
+    
+}
--- ANK/modules/scheduler/scheduler.h
+++ ANK/modules/scheduler/scheduler.h
@@ -1,4 +1,6 @@
 #pragma once
 
 #include "../work_system/work_system.h"
+#include "../Memory_utis/mem_allocer.h"
 
+void sch64(ANK_WORK *works, size64 numof_work);
